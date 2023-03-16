f69a016d909387bb4e2b2b97e45b342086164a37
piedrafita8 <santiagopiedrafita@gmail.com>
2023-03-16 11:01:48 -0300

    feat: add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'grayText': '#777777',
+      'primaryPurple': '#007bff'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
